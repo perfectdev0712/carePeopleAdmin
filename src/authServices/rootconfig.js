04678e3ea262d8943e4f66deec5fa038149bf071
perfectdev0712 <greenland712@outlook.com>
2021-07-01 18:29:00 +0300

    Update rootconfig.js

--- src/authServices/rootconfig.js
+++ src/authServices/rootconfig.js
@@ -1,12 +1,12 @@
 export const Root =
 {
-	adminurl: "http://localhost:5000/api/",
-	admindomain: "http://localhost:5000",
-	imageurl: "http://localhost:5000/",
+	// adminurl: "http://localhost:5000/api/",
+	// admindomain: "http://localhost:5000",
+	// imageurl: "http://localhost:5000/",
 
-	// adminurl: "https://cms.8866casino.net/admin/",
-	// admindomain: "https://cms.8866casino.net",
-	// imageurl: "https://cms.8866casino.net/",
+	adminurl: "https://careshifts.ca/api/",
+	admindomain: "https://careshifts.ca",
+	imageurl: "https://careshifts.ca/",
 
 	token: "careshift-admin",
 	socket: null,
