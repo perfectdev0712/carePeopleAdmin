5ff16854d2521b7761b30036091a2be6fd4f8e05
perfectdev0712 <greenland712@outlook.com>
2021-07-02 17:47:53 +0300

    Update rootconfig.js

--- src/authServices/rootconfig.js
+++ src/authServices/rootconfig.js
@@ -1,12 +1,12 @@
 export const Root =
 {
-	// adminurl: "http://localhost:5000/api/",
-	// admindomain: "http://localhost:5000",
-	// imageurl: "http://localhost:5000/",
+	adminurl: "http://localhost:5000/api/",
+	admindomain: "http://localhost:5000",
+	imageurl: "http://localhost:5000/",
 
-	adminurl: "https://careshifts.ca/api/",
-	admindomain: "https://careshifts.ca",
-	imageurl: "https://careshifts.ca/",
+	// adminurl: "https://careshifts.ca/api/",
+	// admindomain: "https://careshifts.ca",
+	// imageurl: "https://careshifts.ca/",
 
 	token: "careshift-admin",
 	socket: null,
